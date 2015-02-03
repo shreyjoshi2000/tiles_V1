d7dd6da4756ae02b8b2697e08f166e4bccab2961
shreyjoshi2000 <shreyjoshi2000@gmail.com>
2015-02-04 01:01:36 +0545

    V-1.2.2: added form and model

---
+++ tiles_project/src/main/java/com/sj/model/Account.java
@@ -0,0 +1,51 @@
+package com.sj.model;
+
+
+public class Account {
+
+	private int accountid;
+	private String accountHolderName;
+	private String contactNumber;
+	private String address;
+	private double amount;
+	
+	public String getAccountHolderName() {
+		return accountHolderName;
+	}
+	public void setAccountHolderName(String accountHolderName) {
+		this.accountHolderName = accountHolderName;
+	}
+	public String getContactNumber() {
+		return contactNumber;
+	}
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public int getAccountid() {
+		return accountid;
+	}
+	public void setAccountid(int accountid) {
+		this.accountid = accountid;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
